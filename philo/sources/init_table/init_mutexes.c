1afc6fb72e88ed149951cd4375fbf1fb634c8456
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-04-09 15:04:32 +0200

    Safe init_mutexes

--- philo/sources/init_table/init_mutexes.c
+++ philo/sources/init_table/init_mutexes.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 12:44:32 by deydoux           #+#    #+#             */
-/*   Updated: 2024/04/09 13:13:40 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/04/09 15:04:03 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,27 @@ static bool	init_fork_mutexes(t_table *table)
 	while (i < table->seats)
 	{
 		if (pthread_mutex_init(&table->philo[i++].fork.mutex, NULL))
+		{
+			while (i--)
+				pthread_mutex_destroy(&table->philo[i].fork.mutex);
 			return (true);
+		}
 	}
 	return (false);
 }
 
 bool	init_mutexes(t_table *table)
 {
-	if (pthread_mutex_init(&table->mutex, NULL) || init_fork_mutexes(table))
+	bool	error;
+
+	error = pthread_mutex_init(&table->mutex, NULL);
+	if (!error)
 	{
-		ft_putstr_fd(ERR_INIT_MUTEXES, STDERR_FILENO);
-		return (true);
+		error = init_fork_mutexes(table);
+		if (error)
+			pthread_mutex_destroy(&table->mutex);
 	}
-	return (false);
+	if (error)
+		ft_putstr_fd(ERR_INIT_MUTEXES, STDERR_FILENO);
+	return (error);
 }
