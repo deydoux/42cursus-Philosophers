0d2ae9568d6ebf9758aae9d2646e8d4749b5e7ec
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-04-28 20:52:12 +0200

    Routine POC

--- sources/routine/routine.h
+++ sources/routine/routine.h
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:50:56 by deydoux           #+#    #+#             */
-/*   Updated: 2024/04/26 18:09:19 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/04/28 20:46:25 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 # include <stdio.h>
 # include "philo.h"
 
-# define DIE_FORMAT		"%llu %s died\n"
-# define EAT_FORMAT		"%llu %s is eating\n"
-# define FORK_FORMAT	"%llu %s has taken a fork\n"
-# define SLEEP_FORMAT	"%llu %s is sleeping\n"
-# define THINK_FORMAT	"%llu %s is thinking\n"
+# define DIE_FORMAT		"%zu %s died\n"
+# define EAT_FORMAT		"%zu %s is eating\n"
+# define FORK_FORMAT	"%zu %s has taken a fork\n"
+# define SLEEP_FORMAT	"%zu %s is sleeping\n"
+# define THINK_FORMAT	"%zu %s is thinking\n"
 
 int	ms_sleep(unsigned int ms_sec);
 
