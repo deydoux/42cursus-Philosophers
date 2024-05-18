e35de8daf5ce58b7426f0c59cf25212c6e4e4464
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-05-18 21:19:10 +0200

    Update init_philo

--- sources/init_table/init_philos.c
+++ sources/init_table/init_philos.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 12:52:06 by deydoux           #+#    #+#             */
-/*   Updated: 2024/05/13 14:53:44 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/05/18 21:18:47 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 static void	init_philo(t_table *table, size_t i)
 {
-	table->philos[i].even = i % 2 == 0 && i != table->n_philo - 1;
-	table->philos[i].id = i + 1;
-	table->philos[i].left_fork = &table->philos[(i + 1) % table->n_philo]
-		.right_fork;
-	table->philos[i].right_fork.taken = true;
-	table->philos[i].common = &table->common;
+	t_philo	*philo;
+
+	philo = &table->philos[i];
+	philo->even = i % 2 == 0 && i != table->n_philo - 1;
+	philo->id = i + 1;
+	philo->left_fork = &table->philos[(i + 1) % table->n_philo].right_fork;
+	philo->right_fork.taken = true;
+	philo->common = &table->common;
 }
 
 bool	init_philos(t_table *table)
