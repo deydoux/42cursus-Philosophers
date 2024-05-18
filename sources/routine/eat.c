0817b37087cc2b8f5ac938fbd222bedfc3ba2535
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-05-19 00:53:48 +0200

    Fix limit_eat

--- sources/routine/eat.c
+++ sources/routine/eat.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 15:31:51 by deydoux           #+#    #+#             */
-/*   Updated: 2024/05/18 23:24:31 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/05/19 00:53:03 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,7 @@ static bool	take_fork(t_philo_fork *fork, t_philo *philo)
 bool	eat(t_philo *philo)
 {
 	if (take_fork(&philo->right_fork, philo)
-		|| take_fork(philo->left_fork, philo) || (philo->common->limit_eat
-			&& ++philo->eat_count == philo->common->must_eat))
+		|| take_fork(philo->left_fork, philo))
 		return (true);
 	if (philo_print(philo, EAT_FORMAT, &philo->die_time))
 		return (true);
@@ -38,5 +37,8 @@ bool	eat(t_philo *philo)
 	philo_sleep(philo->common->time_to_eat, philo);
 	philo->right_fork.taken = false;
 	philo->left_fork->taken = false;
+	if (philo->common->limit_eat
+		&& ++philo->eat_count == philo->common->must_eat)
+		return (true);
 	return (false);
 }
