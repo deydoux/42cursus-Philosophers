49577567c49c9b428433240eaf7ce01c29ac7278
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-05-19 18:49:47 +0200

    Add even_routine and odd_routine

--- sources/routine/init_routine.c
+++ sources/routine/init_routine.c
@@ -6,23 +6,14 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 18:45:48 by deydoux           #+#    #+#             */
-/*   Updated: 2024/05/19 18:46:05 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/05/19 18:49:34 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "routine.h"
 
-bool	init_routine(t_philo *philo)
+static bool	even_routine(t_philo *philo)
 {
-	printf(THINK_FORMAT, (size_t)0, philo->id);
-	if (!philo->even)
-	{
-		pthread_mutex_unlock(&philo->common->mutex.data);
-		philo->die_time = philo->common->start_time
-			+ philo->common->time_to_die / 1000;
-		philo_sleep(philo->common->time_to_eat / 2, philo);
-		return (eat(philo));
-	}
 	printf(FORK_FORMAT, (size_t)0, philo->id);
 	printf(FORK_FORMAT, (size_t)0, philo->id);
 	printf(EAT_FORMAT, (size_t)0, philo->id);
@@ -33,4 +24,21 @@ bool	init_routine(t_philo *philo)
 	philo->right_fork.taken = false;
 	philo->left_fork->taken = false;
 	return (false);
+}
+
+static bool	odd_routine(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->common->mutex.data);
+	philo->die_time = philo->common->start_time
+		+ philo->common->time_to_die / 1000;
+	philo_sleep(philo->common->time_to_eat / 2, philo);
+	return (eat(philo));
+}
+
+bool	init_routine(t_philo *philo)
+{
+	printf(THINK_FORMAT, (size_t)0, philo->id);
+	if (philo->even)
+		return (even_routine(philo));
+	return (odd_routine(philo));
 }
