8cf9cfbe9638860cba358e946f72a233bd68dee8
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-05-21 14:57:56 +0200

    FIx norm

--- sources/routine/routine.c
+++ sources/routine/routine.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 18:36:35 by deydoux           #+#    #+#             */
-/*   Updated: 2024/05/19 18:52:58 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/05/21 14:57:50 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	*routine(t_philo *philo)
 		return (NULL);
 	}
 	if (!init_routine(philo))
-		while (!sleep_routine(philo) && !think_routine(philo) && !eat_routine(philo))
+		while (!sleep_routine(philo) && !think_routine(philo)
+			&& !eat_routine(philo))
 			;
 	return (NULL);
 }
