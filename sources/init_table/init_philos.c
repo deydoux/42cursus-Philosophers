0e5bb63b5cc1f8d7b36d766618774c950129a24e
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-05-21 15:16:30 +0200

    Add last_even_routine

--- sources/init_table/init_philos.c
+++ sources/init_table/init_philos.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 12:52:06 by deydoux           #+#    #+#             */
-/*   Updated: 2024/05/21 11:46:53 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/05/21 15:05:00 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ static void	init_philo(t_table *table, size_t i)
 	t_philo	*philo;
 
 	philo = &table->philos[i];
-	philo->last = i == table->n_philo - 1 && table->n_philo % 2;
+	philo->last = i == table->n_philo - 1;
 	philo->odd = i % 2;
 	philo->id = i + 1;
 	philo->left_fork = &table->philos[(i + 1) % table->n_philo].right_fork;
-	if (!philo->odd)
+	if (!philo->odd && !philo->last)
 	{
 		philo->right_fork.taken = true;
 		philo->left_fork->taken = true;
--- sources/routine/init_routine.c
+++ sources/routine/init_routine.c
@@ -6,14 +6,24 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 18:45:48 by deydoux           #+#    #+#             */
-/*   Updated: 2024/05/21 11:45:07 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/05/21 15:12:45 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "routine.h"
 
+static bool	last_even_routine(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->common->mutex.data);
+	philo->die_time = philo->common->start_time
+		+ philo->common->time_to_die / 1000;
+	return (eat_routine(philo));
+}
+
 static bool	even_routine(t_philo *philo)
 {
+	if (philo->last)
+		return (last_even_routine(philo));
 	printf(FORK_FORMAT, (size_t)0, philo->id);
 	printf(FORK_FORMAT, (size_t)0, philo->id);
 	printf(EAT_FORMAT, (size_t)0, philo->id);
@@ -31,10 +41,7 @@ static bool	odd_routine(t_philo *philo)
 	pthread_mutex_unlock(&philo->common->mutex.data);
 	philo->die_time = philo->common->start_time
 		+ philo->common->time_to_die / 1000;
-	if (philo->last)
-		philo_sleep(philo->common->time_to_eat * 1.5, philo);
-	else
-		philo_sleep(philo->common->time_to_eat / 2, philo);
+	philo_sleep(philo->common->time_to_eat / 2, philo);
 	return (eat_routine(philo));
 }
 
@@ -46,7 +53,7 @@ bool	init_routine(t_philo *philo)
 		return (true);
 	}
 	printf(THINK_FORMAT, (size_t)0, philo->id);
-	if (philo->odd)
+	if (philo->odd )
 		return (odd_routine(philo));
 	return (even_routine(philo));
 }
