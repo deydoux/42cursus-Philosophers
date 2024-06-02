c0d28a58d25a2582d4edc825655db0b5c85b0c5c
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-06-02 21:59:26 +0200

    Update philo_print

--- philo_bonus/sources/philo_print.c
+++ philo_bonus/sources/philo_print.c
@@ -6,26 +6,35 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 22:40:34 by deydoux           #+#    #+#             */
-/*   Updated: 2024/05/31 22:30:57 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/06/02 21:59:01 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	philo_print(t_philo *philo, char *format, size_t *time_ptr)
+bool	philo_print(t_philo *philo, char *format, size_t *time_ptr)
 {
 	size_t	time;
 
 	sem_wait(philo->write_sem);
+	if (philo->exit)
+	{
+		sem_post(philo->write_sem);
+		return (true);
+	}
 	time = get_ms_time();
 	if (time >= philo->die_time)
 	{
+		sem_post(philo->exit_sem);
 		printf(FORMAT_DIE, time - philo->start_time, philo->id);
-		philo_exit(EXIT_FAILURE, philo);
+		usleep(philo->time_to_die);
+		sem_post(philo->write_sem);
+		return (true);
 	}
 	else if (format)
 		printf(format, time - philo->start_time, philo->id);
 	sem_post(philo->write_sem);
 	if (time_ptr)
 		*time_ptr = time;
+	return (false);
 }
